ba4721a4890cd275f7b5eabd0c8073cb0d77df47
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-04-28 14:04:16 +0200

    ref: command line options moved to a separate module (#212)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // std imports
 use std::{
-    convert::TryFrom,
     default::Default,
     io::{stdin, stdout, IsTerminal},
     path::PathBuf,
@@ -11,23 +10,18 @@ use std::{
 
 // third-party imports
 use chrono::Utc;
-use clap::{ArgAction, CommandFactory, Parser, ValueEnum};
+use clap::{CommandFactory, Parser};
 use itertools::Itertools;
 use nu_ansi_term::Color;
-use once_cell::sync::Lazy;
-use platform_dirs::AppDirs;
-use std::num::NonZeroUsize;
 
 // local imports
 use hl::{
-    app,
+    app, cli, config,
     datefmt::LinuxDateFormat,
     error::*,
     input::InputReference,
-    level::{LevelValueParser, RelaxedLevel},
     output::{OutputStream, Pager},
     query::Query,
-    settings::Settings,
     signal::SignalHandler,
     theme::{Theme, ThemeOrigin},
     timeparse::parse_time,
@@ -37,292 +31,12 @@ use hl::{
 
 // ---
 
-const APP_NAME: &str = "hl";
-
-// ---
-
-/// JSON log converter to human readable representation.
-#[derive(Parser)]
-#[clap(version, disable_help_flag = true)]
-struct Opt {
-    /// Color output options.
-    #[arg(long, default_value = "auto", env = "HL_COLOR", overrides_with = "color")]
-    #[arg(value_enum)]
-    color: ColorOption,
-
-    /// Handful alias for --color=always, overrides --color option.
-    #[arg(short)]
-    color_always: bool,
-
-    /// Output paging options.
-    #[arg(long, default_value = "auto", env = "HL_PAGING", overrides_with = "paging")]
-    #[arg(value_enum)]
-    paging: PagingOption,
-
-    /// Handful alias for --paging=never, overrides --paging option.
-    #[arg(short = 'P')]
-    paging_never: bool,
-
-    /// Color theme.
-    #[arg(
-        long,
-        default_value_t = CONFIG.theme.clone(),
-        env = "HL_THEME",
-        overrides_with="theme",
-    )]
-    theme: String,
-
-    /// Output raw JSON messages instead of formatter messages, it can be useful for applying filters and saving results in original format.
-    #[arg(short, long, overrides_with = "raw")]
-    raw: bool,
-
-    /// Disable raw JSON messages output, overrides --raw option.
-    #[arg(long, overrides_with = "raw")]
-    _no_raw: bool,
-
-    /// Disable unescaping and prettifying of field values.
-    #[arg(long, overrides_with = "raw_fields")]
-    raw_fields: bool,
-
-    /// Allow non-JSON prefixes before JSON messages.
-    #[arg(long, env = "HL_ALLOW_PREFIX", overrides_with = "allow_prefix")]
-    allow_prefix: bool,
-
-    /// Number of interrupts to ignore, i.e. Ctrl-C (SIGINT).
-    #[arg(
-        long,
-        default_value = "3",
-        env = "HL_INTERRUPT_IGNORE_COUNT",
-        overrides_with = "interrupt_ignore_count"
-    )]
-    interrupt_ignore_count: usize,
-
-    /// Buffer size.
-    #[arg(long, default_value = "256 KiB", env="HL_BUFFER_SIZE",  value_parser = parse_non_zero_size, overrides_with="buffer_size")]
-    buffer_size: NonZeroUsize,
-
-    /// Maximum message size.
-    #[arg(long, default_value = "64 MiB", env="HL_MAX_MESSAGE_SIZE",  value_parser = parse_non_zero_size, overrides_with="max_message_size")]
-    max_message_size: NonZeroUsize,
-
-    /// Number of processing threads.
-    #[arg(long, short = 'C', env = "HL_CONCURRENCY", overrides_with = "concurrency")]
-    concurrency: Option<usize>,
-
-    /// Filtering by field values in one of forms [k=v, k~=v, k~~=v, 'k!=v', 'k!~=v', 'k!~~=v'] where ~ does substring match and ~~ does regular expression match.
-    #[arg(short, long, number_of_values = 1)]
-    filter: Vec<String>,
-
-    /// Custom query, accepts expressions from --filter and supports '(', ')', 'and', 'or', 'not', 'in', 'contain', 'like', '<', '>', '<=', '>=', etc.
-    #[arg(short, long, number_of_values = 1)]
-    query: Vec<String>,
-
-    /// Hide or reveal fields with the specified keys, prefix with ! to reveal, specify '!*' to reveal all.
-    #[arg(long, short = 'h', number_of_values = 1)]
-    hide: Vec<String>,
-
-    /// Filtering by level.
-    #[arg(short, long, env = "HL_LEVEL", overrides_with="level", ignore_case=true, value_parser = LevelValueParser)]
-    #[arg(value_enum)]
-    level: Option<RelaxedLevel>,
-
-    /// Filtering by timestamp >= the value (--time-zone and --local options are honored).
-    #[arg(long, allow_hyphen_values = true, overrides_with = "since")]
-    since: Option<String>,
-
-    /// Filtering by timestamp <= the value (--time-zone and --local options are honored).
-    #[arg(long, allow_hyphen_values = true, overrides_with = "until")]
-    until: Option<String>,
-
-    /// Time format, see https://man7.org/linux/man-pages/man1/date.1.html.
-    #[arg(
-        short,
-        long,
-        env="HL_TIME_FORMAT",
-        default_value_t = CONFIG.time_format.clone(),
-        overrides_with = "time_format",
-    )]
-    time_format: String,
-
-    /// Time zone name, see column "TZ identifier" at https://en.wikipedia.org/wiki/List_of_tz_database_time_zones.
-    #[arg(long, short = 'Z', env="HL_TIME_ZONE", default_value = &CONFIG.time_zone.name(), overrides_with="time_zone")]
-    time_zone: chrono_tz::Tz,
-
-    /// Use local time zone, overrides --time-zone option.
-    #[arg(long, short = 'L', overrides_with = "local")]
-    local: bool,
-
-    /// Disable local time zone, overrides --local option.
-    #[arg(long, overrides_with = "local")]
-    _no_local: bool,
-
-    /// Unix timestamp unit.
-    #[arg(
-        long,
-        default_value = "auto",
-        overrides_with = "unix_timestamp_unit",
-        env = "HL_UNIX_TIMESTAMP_UNIT"
-    )]
-    unix_timestamp_unit: UnixTimestampUnit,
-
-    /// Files to process
-    #[arg(name = "FILE")]
-    files: Vec<PathBuf>,
-
-    /// Hide empty fields, applies for null, string, object and array fields only.
-    #[arg(
-        long,
-        short = 'e',
-        env = "HL_HIDE_EMPTY_FIELDS",
-        overrides_with = "hide_empty_fields"
-    )]
-    hide_empty_fields: bool,
-
-    /// Show empty fields, overrides --hide-empty-fields option.
-    #[arg(
-        long,
-        short = 'E',
-        env = "HL_SHOW_EMPTY_FIELDS",
-        overrides_with = "show_empty_fields"
-    )]
-    show_empty_fields: bool,
-
-    /// Show input number and/or input filename before each message.
-    #[arg(long, default_value = "auto", overrides_with = "input_info")]
-    #[arg(value_enum)]
-    input_info: InputInfoOption,
-
-    /// List available themes and exit.
-    #[arg(long)]
-    list_themes: bool,
-
-    /// Sort messages chronologically.
-    #[arg(long, short = 's', overrides_with = "sort")]
-    sort: bool,
-
-    /// Follow input streams and sort messages chronologically during time frame set by --sync-interval-ms option.
-    #[arg(long, short = 'F', overrides_with = "follow")]
-    follow: bool,
-
-    /// Number of last messages to preload from each file in --follow mode.
-    #[arg(long, default_value = "10", overrides_with = "tail")]
-    tail: u64,
-
-    /// Synchronization interval for live streaming mode enabled by --follow option.
-    #[arg(long, default_value = "100", overrides_with = "sync_interval_ms")]
-    sync_interval_ms: u64,
-
-    /// Output file.
-    #[arg(long, short = 'o', overrides_with = "output")]
-    output: Option<String>,
-
-    /// Log message delimiter, [NUL, CR, LF, CRLF] or any custom string.
-    #[arg(long, overrides_with = "delimiter")]
-    delimiter: Option<String>,
-
-    /// Input format.
-    #[arg(
-        long,
-        env = "HL_INPUT_FORMAT",
-        default_value = "auto",
-        overrides_with = "input_format"
-    )]
-    input_format: InputFormat,
-
-    /// Dump index metadata and exit.
-    #[arg(long)]
-    dump_index: bool,
-
-    /// Print debug error messages that can help with troubleshooting.
-    #[arg(long)]
-    debug: bool,
-
-    /// Print help.
-    #[arg(long, default_value_t = false, action = ArgAction::SetTrue)]
-    help: bool,
-}
-
-#[derive(ValueEnum, Debug, Clone, Copy)]
-enum ColorOption {
-    Auto,
-    Always,
-    Never,
-}
-
-#[derive(ValueEnum, Debug, Clone, Copy)]
-enum PagingOption {
-    Auto,
-    Always,
-    Never,
-}
-
-#[derive(ValueEnum, Debug, Clone, Copy)]
-enum InputInfoOption {
-    Auto,
-    None,
-    Full,
-    Compact,
-    Minimal,
-}
-
-#[derive(ValueEnum, Debug, Clone, Copy)]
-enum InputFormat {
-    Auto,
-    Json,
-    Logfmt,
-}
-
-#[derive(ValueEnum, Debug, Clone, Copy)]
-enum UnixTimestampUnit {
-    Auto,
-    S,
-    Ms,
-    Us,
-    Ns,
-}
-
-// ---
-
-static CONFIG: Lazy<Settings> = Lazy::new(|| load_config());
-
-// ---
-
-fn app_dirs() -> AppDirs {
-    AppDirs::new(Some(APP_NAME), true).unwrap()
-}
-
-fn load_config() -> Settings {
-    Settings::load(&app_dirs()).unwrap()
-}
-
-fn parse_size(s: &str) -> std::result::Result<usize, SizeParseError> {
-    match bytefmt::parse(s) {
-        Ok(value) => Ok(usize::try_from(value)?),
-        Err(_) => {
-            if let Ok(value) = bytefmt::parse(s.to_owned() + "ib") {
-                return Ok(usize::try_from(value)?);
-            }
-            Err(SizeParseError::InvalidSize(s.into()))
-        }
-    }
-}
-
-fn parse_non_zero_size(s: &str) -> std::result::Result<NonZeroUsize, NonZeroSizeParseError> {
-    if let Some(value) = NonZeroUsize::new(parse_size(s)?) {
-        Ok(NonZeroUsize::from(value))
-    } else {
-        Err(NonZeroSizeParseError::ZeroSize)
-    }
-}
-
-// ---
-
 fn run() -> Result<()> {
-    let app_dirs = app_dirs();
-    let settings = Settings::load(&app_dirs)?;
-    let opt = Opt::parse();
+    let app_dirs = config::app_dirs();
+    let settings = config::get();
+    let opt = cli::Opt::parse();
     if opt.help {
-        return Opt::command().print_help().map_err(Error::Io);
+        return cli::Opt::command().print_help().map_err(Error::Io);
     }
 
     let color_supported = if stdout().is_terminal() {
@@ -338,14 +52,14 @@ fn run() -> Result<()> {
 
     // Configure color scheme.
     let color = if opt.color_always {
-        ColorOption::Always
+        cli::ColorOption::Always
     } else {
         opt.color
     };
     let use_colors = match color {
-        ColorOption::Auto => stdout().is_terminal() && color_supported,
-        ColorOption::Always => true,
-        ColorOption::Never => false,
+        cli::ColorOption::Auto => stdout().is_terminal() && color_supported,
+        cli::ColorOption::Always => true,
+        cli::ColorOption::Never => false,
     };
     let theme = if use_colors {
         let theme = &opt.theme;
@@ -402,7 +116,7 @@ fn run() -> Result<()> {
     let all = || IncludeExcludeKeyFilter::new(KeyMatchOptions::default());
     let none = || all().excluded();
     let mut fields = all();
-    for (i, key) in CONFIG.fields.hide.iter().chain(&opt.hide).enumerate() {
+    for (i, key) in config::get().fields.hide.iter().chain(&opt.hide).enumerate() {
         if key == "*" {
             fields = none();
         } else if key == "!*" {
@@ -466,26 +180,26 @@ fn run() -> Result<()> {
         filter,
         query,
         fields: hl::FieldOptions {
-            settings: settings.fields,
+            settings: settings.fields.clone(),
             filter: Arc::new(fields),
         },
-        formatting: settings.formatting,
+        formatting: settings.formatting.clone(),
         time_zone: tz,
         hide_empty_fields,
         sort: opt.sort,
         follow: opt.follow,
         sync_interval: Duration::from_millis(opt.sync_interval_ms),
         input_info: match opt.input_info {
-            InputInfoOption::Auto => Some(app::InputInfo::Auto),
-            InputInfoOption::None => None,
-            InputInfoOption::Full => Some(app::InputInfo::Full),
-            InputInfoOption::Compact => Some(app::InputInfo::Compact),
-            InputInfoOption::Minimal => Some(app::InputInfo::Minimal),
+            cli::InputInfoOption::Auto => Some(app::InputInfo::Auto),
+            cli::InputInfoOption::None => None,
+            cli::InputInfoOption::Full => Some(app::InputInfo::Full),
+            cli::InputInfoOption::Compact => Some(app::InputInfo::Compact),
+            cli::InputInfoOption::Minimal => Some(app::InputInfo::Minimal),
         },
         input_format: match opt.input_format {
-            InputFormat::Auto => None,
-            InputFormat::Json => Some(app::InputFormat::Json),
-            InputFormat::Logfmt => Some(app::InputFormat::Logfmt),
+            cli::InputFormat::Auto => None,
+            cli::InputFormat::Json => Some(app::InputFormat::Json),
+            cli::InputFormat::Logfmt => Some(app::InputFormat::Logfmt),
         },
         dump_index: opt.dump_index,
         debug: opt.debug,
@@ -493,11 +207,11 @@ fn run() -> Result<()> {
         tail: opt.tail,
         delimiter,
         unix_ts_unit: match opt.unix_timestamp_unit {
-            UnixTimestampUnit::Auto => None,
-            UnixTimestampUnit::S => Some(app::UnixTimestampUnit::Seconds),
-            UnixTimestampUnit::Ms => Some(app::UnixTimestampUnit::Milliseconds),
-            UnixTimestampUnit::Us => Some(app::UnixTimestampUnit::Microseconds),
-            UnixTimestampUnit::Ns => Some(app::UnixTimestampUnit::Nanoseconds),
+            cli::UnixTimestampUnit::Auto => None,
+            cli::UnixTimestampUnit::S => Some(app::UnixTimestampUnit::Seconds),
+            cli::UnixTimestampUnit::Ms => Some(app::UnixTimestampUnit::Milliseconds),
+            cli::UnixTimestampUnit::Us => Some(app::UnixTimestampUnit::Microseconds),
+            cli::UnixTimestampUnit::Ns => Some(app::UnixTimestampUnit::Nanoseconds),
         },
     });
 
@@ -515,7 +229,7 @@ fn run() -> Result<()> {
         .collect::<Vec<_>>();
     if inputs.len() == 0 {
         if stdin().is_terminal() {
-            let mut cmd = Opt::command();
+            let mut cmd = cli::Opt::command();
             return cmd.print_help().map_err(Error::Io);
         }
         inputs.push(InputReference::Stdin);
@@ -540,15 +254,15 @@ fn run() -> Result<()> {
         .collect::<Result<Vec<_>>>()?;
 
     let paging = match opt.paging {
-        PagingOption::Auto => {
+        cli::PagingOption::Auto => {
             if stdout().is_terminal() {
                 true
             } else {
                 false
             }
         }
-        PagingOption::Always => true,
-        PagingOption::Never => false,
+        cli::PagingOption::Always => true,
+        cli::PagingOption::Never => false,
     };
     let paging = if opt.paging_never || opt.follow { false } else { paging };
     let mut output: OutputStream = match opt.output {
