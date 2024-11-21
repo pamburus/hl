24c16f7cd1952b20af7f5575c2e29388e7d51280
TornaxO7 <tornax@proton.me>
2024-11-21 15:09:32 +0100

    adding flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+        ];
+
+        perSystem = { self', lib, system, pkgs, config, ... }: {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+
+            overlays = with inputs; [
+              rust-overlay.overlays.default
+            ];
+          };
+
+          packages.default = pkgs.callPackage ./nix/package.nix { };
+
+          devShells.default = self'.packages.default;
+        };
+      };
+}
---
+++ nix/package.nix
@@ -0,0 +1,30 @@
+{ lib
+, rustPlatform
+,
+}:
+let
+  cargoToml = builtins.fromTOML (builtins.readFile ../Cargo.toml);
+in
+rustPlatform.buildRustPackage {
+  pname = cargoToml.package.name;
+  version = cargoToml.workspace.package.version;
+
+  src = builtins.path {
+    path = ../.;
+  };
+
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+    outputHashes = {
+      "htp-0.4.2" = "sha256-oYLN0aCLIeTST+Ib6OgWqEgu9qyI0n5BDtIUIIThLiQ=";
+      "wildflower-0.3.0" = "sha256-vv+ppiCrtEkCWab53eutfjHKrHZj+BEAprV5by8plzE=";
+    };
+  };
+
+  meta = {
+    description = cargoToml.package.description;
+    homepage = cargoToml.workspace.package.repository;
+    license = lib.licenses.mit;
+    mainProgram = cargoToml.package.name;
+  };
+}
