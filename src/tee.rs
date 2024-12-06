0dda151f11e1055131b9fee125b1dae3087d3e43
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-12-06 07:47:07 +0100

    feat: better replay for indexed compressed files

--- src/tee.rs
+++ src/tee.rs
@@ -6,12 +6,17 @@ use std::io::{Read, Result, Write};
 pub struct TeeReader<R: Read, W: Write> {
     reader: R,
     writer: W,
+    processed: usize,
 }
 
 impl<R: Read, W: Write> TeeReader<R, W> {
     #[inline]
     pub fn new(reader: R, writer: W) -> TeeReader<R, W> {
-        TeeReader { reader, writer }
+        TeeReader {
+            reader,
+            writer,
+            processed: 0,
+        }
     }
 
     #[inline]
@@ -31,12 +36,18 @@ impl<R: Read, W: Write> TeeReader<R, W> {
     pub fn into(self) -> (R, W) {
         (self.reader, self.writer)
     }
+
+    #[inline]
+    pub fn processed(&self) -> usize {
+        self.processed
+    }
 }
 
 impl<R: Read, W: Write> Read for TeeReader<R, W> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         let n = self.reader.read(buf)?;
         self.writer.write_all(&buf[..n])?;
+        self.processed += n;
         Ok(n)
     }
 }
