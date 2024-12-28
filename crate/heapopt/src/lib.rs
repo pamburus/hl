304833a5aea3b963504dd2fdf25da001048a62f6
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-12-28 15:54:59 +0100

    feat: added `IndexMap` to `heapopt` crate

--- crate/heapopt/src/lib.rs
+++ crate/heapopt/src/lib.rs
@@ -2,6 +2,7 @@
 //! Optimization can be achieved by storing part of the data in a fixed size heapless part.
 //! If that capacity is not enough, the rest is stored in a heap allocated part.
 
+pub mod indexmap;
 pub mod vec;
 
 /// Vec is a re-export of the [`vec::Vec`]`.
@@ -12,3 +13,12 @@ pub type VecIter<'a, T> = vec::Iter<'a, T>;
 
 /// VecIterMut is a re-export of the [`vec::IterMut`]`.
 pub type VecIterMut<'a, T> = vec::IterMut<'a, T>;
+
+/// IndexMap is a re-export of the [`indexmap::IndexMap`]`.
+pub type IndexMap<K, V, const N: usize> = indexmap::IndexMap<K, V, N>;
+
+/// IndexMapIter is a re-export of the [`indexmap::Iter`]`.
+pub type IndexMapIter<'a, K, V> = indexmap::Iter<'a, K, V>;
+
+/// IndexMapIterMut is a re-export of the [`indexmap::IterMut`]`.
+pub type IndexMapIterMut<'a, K, V> = indexmap::IterMut<'a, K, V>;
