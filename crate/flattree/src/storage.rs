7ef1e1140383109884a22655b19002c951df26dd
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-12-28 16:53:28 +0100

    draft 1

---
+++ crate/flattree/src/storage.rs
@@ -0,0 +1,79 @@
+use std::vec::Vec;
+
+pub trait StorageType {
+    type Value;
+    type Storage<T>: Storage<T>;
+}
+
+impl<T> StorageType for Vec<T> {
+    type Value = T;
+    type Storage<U> = Vec<U>;
+}
+
+pub trait Storage<T>: Default {
+    fn len(&self) -> usize;
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    fn get(&self, index: usize) -> Option<&T>;
+    fn get_mut(&mut self, index: usize) -> Option<&mut T>;
+    fn set(&mut self, index: usize, item: T) -> Option<T>;
+    fn push(&mut self, item: T);
+    fn clear(&mut self);
+}
+
+impl<T> Storage<T> for Vec<T> {
+    #[inline]
+    fn len(&self) -> usize {
+        Vec::len(self)
+    }
+
+    #[inline]
+    fn get(&self, index: usize) -> Option<&T> {
+        self.as_slice().get(index)
+    }
+
+    #[inline]
+    fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        self.as_mut_slice().get_mut(index)
+    }
+
+    #[inline]
+    fn set(&mut self, index: usize, item: T) -> Option<T> {
+        self.as_mut_slice()
+            .get_mut(index)
+            .map(|old| std::mem::replace(old, item))
+    }
+
+    #[inline]
+    fn push(&mut self, item: T) {
+        Vec::push(self, item)
+    }
+
+    #[inline]
+    fn clear(&mut self) {
+        Vec::clear(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_storage() {
+        let mut storage = Vec::new();
+        assert_eq!(storage.len(), 0);
+        assert!(storage.is_empty());
+        storage.push(1);
+        assert_eq!(storage.len(), 1);
+        assert!(!storage.is_empty());
+        assert_eq!(storage.get(0), Some(&1));
+        assert_eq!(storage.get_mut(0), Some(&mut 1));
+        assert_eq!(storage.set(0, 2), Some(1));
+        assert_eq!(storage.get(0), Some(&2));
+        storage.clear();
+        assert_eq!(storage.len(), 0);
+        assert!(storage.is_empty());
+    }
+}
---
+++ crate/flattree/src/tree.rs
@@ -0,0 +1,212 @@
+use crate::storage::{Storage, StorageType};
+
+pub struct FlatTree<S>
+where
+    S: StorageType,
+{
+    s: S::Storage<Item<S::Value>>,
+}
+
+impl<S> FlatTree<S>
+where
+    S: StorageType,
+    S::Storage<Item<S::Value>>: Default,
+{
+    pub fn build() -> FlatTreeBuilder<S> {
+        FlatTreeBuilder::new(Default::default())
+    }
+}
+
+impl<S> FlatTree<S>
+where
+    S: StorageType,
+{
+    pub fn build_with(storage: S::Storage<Item<S::Value>>) -> FlatTreeBuilder<S> {
+        FlatTreeBuilder::new(storage)
+    }
+}
+
+pub struct FlatTreeBuilder<S>
+where
+    S: StorageType,
+{
+    s: S::Storage<Item<S::Value>>,
+}
+
+impl<S> FlatTreeBuilder<S>
+where
+    S: StorageType,
+{
+    pub fn new(storage: S::Storage<Item<S::Value>>) -> Self {
+        Self { s: storage }
+    }
+
+    pub fn root<'b>(&'b mut self) -> NodeBuilder<'b, Self> {
+        NodeBuilder {
+            b: self,
+            parent: None,
+            ld: 0,
+        }
+    }
+
+    pub fn done(self) -> FlatTree<S> {
+        FlatTree { s: self.s }
+    }
+
+    pub fn add(&mut self, value: S::Value) -> &mut Self {
+        self.s.push(Item::new(value));
+        self
+    }
+
+    fn update(&mut self, index: usize, f: impl FnOnce(&mut Item<S::Value>)) -> &mut Self {
+        f(self.s.get_mut(index).unwrap());
+        self
+    }
+}
+
+impl<S> From<FlatTreeBuilder<S>> for FlatTree<S>
+where
+    S: StorageType,
+{
+    fn from(builder: FlatTreeBuilder<S>) -> Self {
+        builder.done()
+    }
+}
+
+// ---
+
+pub trait Build<S: StorageType>
+where
+    Self: Sized,
+{
+    fn add(self, value: S::Value) -> Self;
+    fn build(self, value: S::Value, f: impl FnOnce(Self) -> Self) -> Self;
+}
+
+// ---
+
+pub struct NodeBuilder<'b, TB> {
+    b: &'b mut TB,
+    parent: Option<usize>,
+    ld: usize,
+}
+
+impl<'b, S> NodeBuilder<'b, FlatTreeBuilder<S>>
+where
+    S: StorageType,
+{
+    pub fn add(&mut self, value: S::Value) -> &mut Self {
+        self.b.s.push(Item {
+            parent: self.parent,
+            ..Item::new(value)
+        });
+        self.ld += 1;
+        self
+    }
+
+    fn end(self) -> &'b mut FlatTreeBuilder<S> {
+        if let Some(index) = self.parent {
+            let lf = self.b.s.len() - index;
+            let ld = self.ld;
+            self.b.update(index, |item| {
+                item.lf = lf;
+                item.ld = ld;
+            })
+        } else {
+            self.b
+        }
+    }
+
+    fn snapshot(self) -> (NodeBuilderSnapshot, &'b mut FlatTreeBuilder<S>) {
+        (
+            NodeBuilderSnapshot {
+                parent: self.parent,
+                ld: self.ld,
+            },
+            self.b,
+        )
+    }
+}
+
+impl<'b, S> Build<S> for NodeBuilder<'b, FlatTreeBuilder<S>>
+where
+    S: StorageType,
+{
+    #[inline]
+    fn add(mut self, value: S::Value) -> Self {
+        NodeBuilder::add(&mut self, value);
+        self
+    }
+
+    #[inline]
+    fn build(mut self, value: S::Value, f: impl FnOnce(Self) -> Self) -> Self {
+        let index = self.b.s.len();
+        NodeBuilder::add(&mut self, value);
+
+        let (snapshot, b) = self.snapshot();
+
+        let child = NodeBuilder {
+            b,
+            parent: Some(index),
+            ld: 0,
+        };
+
+        let b = f(child).end();
+
+        (snapshot, b).into()
+    }
+}
+
+impl<'b, S> From<(NodeBuilderSnapshot, &'b mut FlatTreeBuilder<S>)> for NodeBuilder<'b, FlatTreeBuilder<S>>
+where
+    S: StorageType,
+{
+    #[inline]
+    fn from((state, b): (NodeBuilderSnapshot, &'b mut FlatTreeBuilder<S>)) -> Self {
+        Self {
+            b,
+            parent: state.parent,
+            ld: state.ld,
+        }
+    }
+}
+
+struct NodeBuilderSnapshot {
+    parent: Option<usize>,
+    ld: usize,
+}
+
+pub struct Item<T> {
+    value: T,
+    parent: Option<usize>, // index of parent
+    lf: usize,             // length (flat) - nubmer of direct and indirect children
+    ld: usize,             // length (direct) - number of direct children
+}
+
+impl<T> Item<T> {
+    fn new(value: T) -> Self {
+        Self {
+            value,
+            parent: None,
+            lf: 0,
+            ld: 0,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut builder = FlatTree::<Vec<_>>::build();
+        builder
+            .root()
+            .add(1)
+            .add(2)
+            .build(3, |b| b.add(4).add(5).build(6, |b| b.add(7).add(8)));
+        let tree = builder.done();
+        assert_eq!(tree.s.len(), 8);
+    }
+}
