65d13965c0efb3532c740ae2e0d0fadc10c4684b
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-01-01 19:24:53 +0100

    simplified build trait

--- src/model/v2/ast.rs
+++ src/model/v2/ast.rs
@@ -53,13 +53,18 @@ pub type Children<'s> = tree::Children<'s, Value<'s>>;
 
 impl<'s, T: tree::Build<Value = Value<'s>>> Build<'s> for T {}
 
-pub trait BuildExt<'s> where Self: Sized {
+pub trait BuildExt<'s>
+where
+    Self: Sized,
+{
     type Child: BuildExt<'s>;
 
     fn add_scalar(self, scalar: Scalar<'s>) -> Self;
-    fn add_object(self, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self>;
-    fn add_array(self, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self>;
-    fn add_field(self, key: String<'s>, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self>;
+    fn add_composite(
+        self,
+        composite: Composite<'s>,
+        f: impl FnOnce(Self::Child) -> Result<Self::Child>,
+    ) -> Result<Self>;
 }
 
 impl<'s, T> BuildExt<'s> for T
@@ -74,18 +79,12 @@ where
     }
 
     #[inline]
-    fn add_object(self, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self> {
-        self.build_e(Value::Object, f)
-    }
-
-    #[inline]
-    fn add_array(self, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self> {
-        self.build_e(Value::Array, f)
-    }
-
-    #[inline]
-    fn add_field(self, key: String<'s>, f: impl FnOnce(Self::Child) -> Result<Self::Child>) -> Result<Self> {
-        self.build_e(Value::Key(key), f)
+    fn add_composite(
+        self,
+        composite: Composite<'s>,
+        f: impl FnOnce(Self::Child) -> Result<Self::Child>,
+    ) -> Result<Self> {
+        self.build_e(composite.into(), f)
     }
 }
 
@@ -101,10 +100,61 @@ pub type String<'s> = EncodedString<'s>;
 #[derive(Debug, Clone, Copy)]
 pub enum Value<'s> {
     Scalar(Scalar<'s>),
-    Array,
-    Object,
-    Key(String<'s>),
+    Composite(Composite<'s>),
 }
+
+impl<'s> Value<'s> {
+    #[inline]
+    pub const fn null() -> Self {
+        Self::Scalar(Scalar::Null)
+    }
+
+    #[inline]
+    pub const fn bool(value: bool) -> Self {
+        Self::Scalar(Scalar::Bool(value))
+    }
+
+    #[inline]
+    pub const fn number(value: &'s str) -> Self {
+        Self::Scalar(Scalar::Number(value))
+    }
+
+    #[inline]
+    pub const fn string(value: String<'s>) -> Self {
+        Self::Scalar(Scalar::String(value))
+    }
+
+    #[inline]
+    pub const fn array() -> Self {
+        Self::Composite(Composite::Array)
+    }
+
+    #[inline]
+    pub const fn object() -> Self {
+        Self::Composite(Composite::Object)
+    }
+
+    #[inline]
+    pub const fn field(value: String<'s>) -> Self {
+        Self::Composite(Composite::Field(value))
+    }
+}
+
+impl<'s> From<Scalar<'s>> for Value<'s> {
+    #[inline]
+    fn from(scalar: Scalar<'s>) -> Self {
+        Self::Scalar(scalar)
+    }
+}
+
+impl<'s> From<Composite<'s>> for Value<'s> {
+    #[inline]
+    fn from(composite: Composite<'s>) -> Self {
+        Self::Composite(composite)
+    }
+}
+
+// ---
 
 #[derive(Debug, Clone, Copy)]
 pub enum Scalar<'s> {
@@ -112,4 +162,11 @@ pub enum Scalar<'s> {
     Bool(bool),
     Number(&'s str),
     String(String<'s>),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Composite<'s> {
+    Array,
+    Object,
+    Field(String<'s>),
 }
