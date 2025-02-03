c7912f2a88092bda11c2788ff9a3b8738474b1fb
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-02-03 21:41:25 +0100

    feat: mem benchmark restructured

--- benches/all/mem.rs
+++ benches/all/mem.rs
@@ -1,11 +1,17 @@
+// std imports
+use std::{hint::black_box, time::Duration};
+
 // third-party imports
-use criterion::{criterion_group, Criterion};
-use std::hint::black_box;
+use criterion::{criterion_group, BenchmarkId, Criterion, Throughput};
 
-criterion_group!(benches, benchmark);
+criterion_group!(benches, bench);
 
-fn benchmark(c: &mut Criterion) {
-    let mut c = c.benchmark_group("mem");
+const GROUP: &str = "mem";
+
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group(GROUP);
+    group.warm_up_time(Duration::from_millis(250));
+    group.measurement_time(Duration::from_secs(2));
 
     let bufs = |size| {
         let vi: Vec<u8> = (0..size).into_iter().map(|x| x as u8).collect();
@@ -14,39 +20,48 @@ fn benchmark(c: &mut Criterion) {
     };
 
     for n in [512, 4096] {
-        c.bench_function(format!("mem-rotate-{}", n), |b| {
+        group.throughput(Throughput::Bytes(n as u64));
+
+        group.bench_function(BenchmarkId::new("rotate", n), |b| {
             let (mut vi, _) = bufs(n);
             b.iter(|| {
                 black_box(&mut vi).rotate_right(1);
             });
         });
-        c.bench_function(format!("mem-copy-{}", n), |b| {
+
+        group.bench_function(BenchmarkId::new("copy", n), |b| {
             let (vi, mut ve) = bufs(n);
             b.iter(|| {
                 ve.clear();
-                black_box(&mut ve).extend_from_slice(black_box(&vi).as_slice());
+                black_box(&mut ve).extend_from_slice(black_box(&vi.as_slice()));
             });
         });
     }
 
-    c.bench_function("mem-find-single-value-4096", |b| {
-        let vi: Vec<u8> = (0..4096).into_iter().map(|x| (x / 16) as u8).collect();
-        b.iter(|| {
-            black_box(vi.iter().position(|&x| x == 128));
-        });
-    });
+    for n in [4096] {
+        group.throughput(Throughput::Bytes(n as u64));
 
-    c.bench_function("mem-find-one-of-two-values-4096", |b| {
-        let vi: Vec<u8> = (0..4096).into_iter().map(|x| (x / 16) as u8).collect();
-        b.iter(|| {
-            black_box(vi.iter().position(|&x| matches!(x, 128 | 192)));
-        });
-    });
+        let setup = || (0..n).into_iter().map(|x| (x * 256 / n) as u8).collect::<Vec<u8>>();
 
-    c.bench_function("mem-find-one-of-four-values-4096", |b| {
-        let vi: Vec<u8> = (0..4096).into_iter().map(|x| (x / 16) as u8).collect();
-        b.iter(|| {
-            black_box(vi.iter().position(|&x| matches!(x, 128 | 192 | 224 | 240)));
+        group.bench_function(BenchmarkId::new("find-single-value", n), |b| {
+            let needle = 128;
+            b.iter_with_setup(setup, |vi| {
+                black_box(vi.iter().position(|&x| x == needle));
+            });
         });
-    });
+
+        group.bench_function(BenchmarkId::new("find-one-of-two-values", n), |b| {
+            b.iter_with_setup(setup, |vi| {
+                black_box(vi.iter().position(|&x| matches!(x, 128 | 192)));
+            });
+        });
+
+        group.bench_function(BenchmarkId::new("find-one-of-four-values", n), |b| {
+            b.iter_with_setup(setup, |vi| {
+                black_box(vi.iter().position(|&x| matches!(x, 128 | 192 | 224 | 240)));
+            });
+        });
+    }
+
+    group.finish();
 }
