023de3422ad1e715126738d564d58f9178e41a7c
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-07-11 01:05:32 +0200

    fix(contrib): termframe.sh: fixed typo

--- contrib/bin/termframe.sh
+++ contrib/bin/termframe.sh
@@ -3,7 +3,7 @@
 if command -v termframe &> /dev/null; then
     TERMFRAME=$(command -v termframe)
 else
-    TERMFRAME=cargo run --lcoked --release --git github.com/pamburus/termframe --
+    TERMFRAME=cargo run --locked --release --git github.com/pamburus/termframe --
 fi
 
 ${TERMFRAME:?} "$@"
