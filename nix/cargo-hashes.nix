ddd66e848c8b848cfe2c9a9badb8ae1269179e70
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-09-13 19:30:50 +0200

    build(nix): move cargo hashes to separate file

---
+++ nix/cargo-hashes.nix
@@ -0,0 +1,7 @@
+# Cargo output hashes for git dependencies.
+# This file centralizes the management of output hashes for git dependencies
+# used in Cargo.lock. Update these hashes when dependencies change.
+{
+  "htp-0.4.2" = "sha256-oYLN0aCLIeTST+Ib6OgWqEgu9qyI0n5BDtIUIIThLiQ=";
+  "wildflower-0.3.0" = "sha256-vv+ppiCrtEkCWab53eutfjHKrHZj+BEAprV5by8plzE=";
+}
