a2fc7a368b8672931ae1b9b7936bec3be35aace6
pamburus <12778560+pamburus@users.noreply.github.com>
2025-09-18 06:34:52 +0000

    nix: update binary hashes for v0.32.0

--- nix/binary-hashes.nix
+++ nix/binary-hashes.nix
@@ -13,7 +13,7 @@
     "hl-linux-x86_64-musl.tar.gz" = "sha256-NvatH7J5/xjd/vmhE6CKe6BIC17b4+AQIS2OOFDgUxA=";
     "hl-linux-arm64-musl.tar.gz" = "sha256-lbmJF9ECksCL/A0QMysTyQjOwVLi4hyOB0uFzpYA1Cs=";
     "hl-macos-x86_64.tar.gz" = "sha256-kcf18Us6KKAzdhTv98ashNTBJRDq1dBEJztJMoOUnso=";
-    "hl-macos-arm64.tar.gz" = "sha256-kcf18Us6KKAzdhTv98ashNTBJRDq1dBEJztJMoOUnso=";
+    "hl-macos-arm64.tar.gz" = "sha256-1rXrkfEskNErb1Tf7IepyerUPsyQJ1gpR+5IScJCTcg=";
   };
   # New versions will be automatically added here by GitHub Actions
 }
