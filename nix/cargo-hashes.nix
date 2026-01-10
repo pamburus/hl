2a07fe726bbe6036fdb16369611386f2034c858c
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2026-01-10 15:00:42 +0100

    build(deps): remove wildflower dependency

--- nix/cargo-hashes.nix
+++ nix/cargo-hashes.nix
@@ -3,5 +3,4 @@
 # used in Cargo.lock. Update these hashes when dependencies change.
 {
   "clap_mangen-0.2.31" = "sha256-9PqsAM9akOnIWtuEZd0BHy1oMz7JIs/qBQzBwjznuF0=";
-  "wildflower-0.3.0" = "sha256-vv+ppiCrtEkCWab53eutfjHKrHZj+BEAprV5by8plzE=";
 }
