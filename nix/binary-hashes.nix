ed445c8057a00e1b864914978a4dc74680918f4f
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2026-01-18 17:32:11 +0100

    build(nix): switch to nixos-25.11

--- nix/binary-hashes.nix
+++ nix/binary-hashes.nix
@@ -1,8 +1,12 @@
 {
   "0.35.2" = {
-    "hl-linux-arm64-musl.tar.gz" = "sha256-/LV/SfMZGOtwh0zHV0swTPlvtqEoa5wwlqeNA/W09ng=";
-    "hl-linux-x86_64-musl.tar.gz" = "sha256-P2mvwi8nTulJxnMYvTOhDr2iuVNJYVZTTTsQVIKccL0=";
-    "hl-macos-arm64.tar.gz" = "sha256-Qvbl/v/AlLQQU7+lYMyNn67m3nhmTNWVv6N/kscqSEU=";
-    "hl-macos-x86_64.tar.gz" = "sha256-UQfXsdkce6YwsSa5lnmWVZIi/x2QMCrK1v7J5IegxpU=";
+    "hl-linux-arm64-musl.tar.gz" =
+      "sha256-/LV/SfMZGOtwh0zHV0swTPlvtqEoa5wwlqeNA/W09ng=";
+    "hl-linux-x86_64-musl.tar.gz" =
+      "sha256-P2mvwi8nTulJxnMYvTOhDr2iuVNJYVZTTTsQVIKccL0=";
+    "hl-macos-arm64.tar.gz" =
+      "sha256-Qvbl/v/AlLQQU7+lYMyNn67m3nhmTNWVv6N/kscqSEU=";
+    "hl-macos-x86_64.tar.gz" =
+      "sha256-UQfXsdkce6YwsSa5lnmWVZIi/x2QMCrK1v7J5IegxpU=";
   };
 }
--- nix/binary-package.nix
+++ nix/binary-package.nix
@@ -11,11 +11,13 @@ let
   availableVersions = builtins.attrNames releaseHashes;
 
   # Fail if no versions are available in binary-hashes.nix
-  version = if availableVersions == []
-           then throw "No versions available in binary-hashes.nix - package configuration is inconsistent"
-           else builtins.foldl' (acc: v:
-             if builtins.compareVersions v acc > 0 then v else acc
-           ) (builtins.head availableVersions) availableVersions;
+  version = if availableVersions == [ ] then
+    throw
+    "No versions available in binary-hashes.nix - package configuration is inconsistent"
+  else
+    builtins.foldl'
+    (acc: v: if builtins.compareVersions v acc > 0 then v else acc)
+    (builtins.head availableVersions) availableVersions;
 
   # Map Nix system to asset name
   getAssetName = system:
@@ -28,11 +30,12 @@ let
 
   # Build the download URL
   assetName = getAssetName stdenv.hostPlatform.system;
-  downloadUrl = "https://github.com/pamburus/hl/releases/download/v${version}/${assetName}";
+  downloadUrl =
+    "https://github.com/pamburus/hl/releases/download/v${version}/${assetName}";
 
   # Get hash for the current version and asset
-  assetHash = releaseHashes.${version}.${assetName} or
-    (throw "No hash available for version ${version} and asset ${assetName} - package configuration is inconsistent");
+  assetHash = releaseHashes.${version}.${assetName} or (throw
+    "No hash available for version ${version} and asset ${assetName} - package configuration is inconsistent");
 
   # Fetch the binary
   src = fetchurl {
@@ -40,8 +43,7 @@ let
     sha256 = assetHash;
   };
 
-in
-stdenv.mkDerivation {
+in stdenv.mkDerivation {
   pname = "hl-bin";
   inherit version;
 
@@ -92,14 +94,13 @@ stdenv.mkDerivation {
     homepage = cargoToml.workspace.package.repository;
     license = lib.licenses.mit;
     changelog = "${cargoToml.workspace.package.repository}/releases";
-    maintainers = [
-      {
-        name = "Pavel Ivanov";
-        github = "pamburus";
-        email = "mr.pavel.ivanov@gmail.com";
-      }
-    ];
-    platforms = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    maintainers = [{
+      name = "Pavel Ivanov";
+      github = "pamburus";
+      email = "mr.pavel.ivanov@gmail.com";
+    }];
+    platforms =
+      [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     mainProgram = "hl";
   };
 }
