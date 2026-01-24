dda27daf12fe0ec9c39feedb26ab2225741e243a
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2026-01-24 12:49:30 +0100

    style(nix): use rfc-style nixfmt (#1292)

--- nix/package.nix
+++ nix/package.nix
@@ -1,4 +1,10 @@
-{ lib, stdenv, makeRustPlatform, rust-bin, installShellFiles, }:
+{
+  lib,
+  stdenv,
+  makeRustPlatform,
+  rust-bin,
+  installShellFiles,
+}:
 let
   cargoToml = builtins.fromTOML (builtins.readFile ../Cargo.toml);
   toolchain = rust-bin.fromRustupToolchainFile ../rust-toolchain.toml;
@@ -6,7 +12,8 @@ let
     cargo = toolchain;
     rustc = toolchain;
   };
-in rustPlatform.buildRustPackage {
+in
+rustPlatform.buildRustPackage {
   pname = cargoToml.package.name;
   version = cargoToml.workspace.package.version;
 
@@ -39,11 +46,13 @@ in rustPlatform.buildRustPackage {
     homepage = cargoToml.workspace.package.repository;
     license = lib.licenses.mit;
     changelog = "${cargoToml.workspace.package.repository}/releases";
-    maintainers = [{
-      name = "Pavel Ivanov";
-      github = "pamburus";
-      email = "mr.pavel.ivanov@gmail.com";
-    }];
+    maintainers = [
+      {
+        name = "Pavel Ivanov";
+        github = "pamburus";
+        email = "mr.pavel.ivanov@gmail.com";
+      }
+    ];
     platforms = lib.platforms.unix ++ lib.platforms.windows;
     mainProgram = cargoToml.package.name;
     categories = cargoToml.package.categories or [ ];
