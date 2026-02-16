f4f3af3777a1748e307fba461250b70d50057f72
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2026-02-16 22:36:41 +0100

    config

--- src/output.rs
+++ src/output.rs
@@ -6,7 +6,7 @@ use serde::Deserialize;
 pub type OutputStream = Box<dyn Write + Send + Sync>;
 
 #[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "kebab-case")]
 pub enum OutputDelimiter {
     Newline,
     Nul,
