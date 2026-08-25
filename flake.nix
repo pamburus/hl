83d41811703b9e8ddcb0b7856c98805890430893
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2026-08-26 00:33:18 +0200

    build(nix): update nixpkgs to nixos-26.05 (#1541)
    * Development and packaging environments now track the **NixOS 26.05** release channel

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
